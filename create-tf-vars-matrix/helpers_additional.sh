6ab4e0b61ec59257f55c1f88e8e3760978eaf1ea
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2023-09-28 15:26:05 +0200

    feat: allow failing operations for environment - A new optional flag `allow-failing-terraform-operations` is introduced in the workflow input `environments-yml`. The flag is specified on a per environment basis. Default value is `false`. - If the flag is set to `true` for an environment the workflow will not terminate upon failing terraform operations. Failing steps will also be ignored in the workflows conclusion step.
    final?

--- create-tf-vars-matrix/helpers_additional.sh
+++ create-tf-vars-matrix/helpers_additional.sh
@@ -9,6 +9,8 @@ function has-field { if [[ "$(echo "${ENVIRONMENT_OBJ}" | jq --arg name "$1" 'ha
 function is-yml-input { if [[ " ${YML_INPUTS[*]} " =~ " ${1} " ]]; then true; else false; fi; }
 function set-field { ENVIRONMENT_OBJ=$(echo "${ENVIRONMENT_OBJ}" | jq --arg name "$1" --arg value "$2" '.[$name] = $value'); }
 function set-field-from-json { ENVIRONMENT_OBJ=$(echo "${ENVIRONMENT_OBJ}" | jq --arg name "$1" --argjson json_value "$2" '.[$name] = $json_value'); }
+function set-bool-field-true { set-field-from-json "$1" "true"; } # really just an alias
+function set-bool-field-false { set-field-from-json "$1" "false"; } # really just an alias
 function get-val { echo "${ENVIRONMENT_OBJ}" | jq -r --arg name "${1}" '.[$name] | select( . != null )'; }
 function rm-field { ENVIRONMENT_OBJ=$(echo "${ENVIRONMENT_OBJ}" | jq --arg key_name "$1" 'del(.[$key_name])'); }
 function _jjq { echo ${ENV_VARS} | base64 --decode | jq -r ${*}; }
