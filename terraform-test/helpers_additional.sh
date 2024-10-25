c43d8cb37216e9dbfe28c6ba0060c610494d1b4f
Artjoms Laivins <38862673+Artlvns@users.noreply.github.com>
2024-10-25 10:03:41 +0200

    Update terraform-test/helpers_additional.sh
    Co-authored-by: Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>

--- terraform-test/helpers_additional.sh
+++ terraform-test/helpers_additional.sh
@@ -5,5 +5,5 @@ function printSection { echo -e "\n\n${1}\n$(getSeparator)\n"; }
 function queryStatus {
   local test_run=$1
   local json_file=$2
-  jq --arg test_run "$test_run" '. | select(.type == "test_run") | select(.test_run.run == $test_run) | select(.test_run.progress == "complete") | .test_run.status' ${json_file}
+  jq --arg test_run "$test_run" '. | select(.type == "test_run") | select(.test_run.run == $test_run) | select(.test_run.progress == "complete") | .test_run.status' "${json_file}"
 }
