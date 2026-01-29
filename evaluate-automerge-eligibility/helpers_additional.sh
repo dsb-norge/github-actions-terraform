00ba547fad8f30078485c2a5072ffdaa3cfcaf1e
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2026-01-29 11:49:51 +0100

    wip: more fancy automerge

---
+++ evaluate-automerge-eligibility/helpers_additional.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+# additional helper functions will be added here
+# ==========================================================
+
+
+# ==========================================================
+log-info "'$(basename ${BASH_SOURCE[0]})' loaded."
